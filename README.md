ea99ef104426397b3d1df1ce2ab9112bc23dcaa2
B4n4n4joe <Marcel.Soler.Lopez@gmail.com>
2018-10-06 14:37:57 +0200

    Newest commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python_test
 This is an example project to demonstrate the use of git in Ubuntu
+change
--- py_test.py
+++ py_test.py
@@ -2,5 +2,6 @@
 
 def main(msg)
 	#Adding the second comment"
+    #Adding another comment
     print(msg)
 main("hello world")
